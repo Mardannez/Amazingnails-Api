75c421042d07cf8de2854b4e7875847652ae38aa
Mardannez <dantrochez88@gmail.com>
2026-01-05 15:27:32 -0600

    Fix vercel routing: Sigle api function  + prisma generate in build

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,11 @@
-// api/index.ts
+import type { VercelRequest, VercelResponse } from "@vercel/node";
 import app from "../src/app";
-export default app;
+
+// recomendado para Express (sin bodyParser de Vercel)
+export const config = {
+  api: { bodyParser: false },
+};
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  return app(req as any, res as any);
+}
